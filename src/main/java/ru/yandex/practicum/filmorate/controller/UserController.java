04d4b0db353cfe2c894640dcd8d7577836621346
Kulkov_Mikhail <mk1221@yandex.ru>
2024-02-29 00:51:05 +0300

    upd: User endpoints

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,10 +1,29 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    List<User> users = new ArrayList<>();
 
+    @GetMapping
+    public List<User> listAllUsers() {
+        return users;
+    }
+
+    @PostMapping
+    public void create(@RequestBody User user) {
+        users.add(user);
+    }
+
+    @PutMapping
+    public void update(@RequestBody User user) {
+        users.remove(user);
+        users.add(user);
+    }
 }
