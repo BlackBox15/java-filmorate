1abe6e0668b4724c18ef7c588a6d1d83ee6c91bd
Kulkov_Mikhail <mk1221@yandex.ru>
2024-02-29 01:03:51 +0300

    upd: validate()

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,5 +28,21 @@ public class UserController {
         users.remove(user);
         users.add(user);
         return user;
+    }
+
+    private boolean validateUser(User user) {
+        if (user.getBirthday().isAfter(LocalDateTime.now())) return false;                  // дата рождения не может быть в будущем
+        if (user.getEmail().isEmpty() || !(user.getEmail().contains("@"))) return false;  // электронная почта не может быть пустой и должна содержать символ @
+        if (user.getLogin().isEmpty() || user.getLogin().matches("\\*\\s\\*")) return false;
+        if (user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+
+
+
+
+
+
+        return true;
     }
 }
