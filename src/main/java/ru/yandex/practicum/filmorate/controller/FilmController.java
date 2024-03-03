3a4f461093b44e596c2b1592d420d5c7320089c8
Kulkov_Mikhail <mk1221@yandex.ru>
2024-03-03 19:55:47 +0300

    upd: добавление эндпоинта для получения всех фильмов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,11 +1,30 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
+    List<Film> films = new ArrayList<>();
+
+    /**
+     * добавление фильма;
+     * обновление фильма;
+     * .
+     */
+
+    // получение всех фильмов
+    @GetMapping
+    public List<Film> listAllUsers() {
+        return films;
+    }
+
 
 }
 
