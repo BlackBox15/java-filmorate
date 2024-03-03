0a5c8d56867a4429e1c1266fa160b7fe898ff9ec
Kulkov_Mikhail <mk1221@yandex.ru>
2024-03-03 19:57:50 +0300

    upd: эндпоинты для добавление фильмы, для обновления фильма

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.ArrayList;
@@ -25,6 +23,26 @@ public class FilmController {
         return films;
     }
 
+    @PostMapping
+    public void create(@RequestBody Film film) throws InvalidEmailException {
+        if (user.hashCode() == 0) {
+            throw new InvalidEmailException("В переданных данных отсутствует адрес электронной почты");
+        }
+        if(users.stream().anyMatch(item -> item.equals(user))) {
+            throw new InvalidEmailException("Пользователь с указанным адресом электронной почты уже был добавлен ранее");
+        }
 
+        films.add(film);
+    }
+
+    @PutMapping
+    public void update(@RequestBody Film film) throws InvalidEmailException {
+        if (user.hashCode() == 0) {
+            throw new InvalidEmailException("В переданных данных отсутствует адрес электронной почты");
+        }
+
+        films.remove(film);
+        films.add(film);
+    }
 }
 
