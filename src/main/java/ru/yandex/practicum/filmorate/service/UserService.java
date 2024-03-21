a9756c15bd3a29a5c5e255b05e60cb450c02e0b3
Kulkov_Mikhail <mk1221@yandex.ru>
2024-03-21 15:21:00 +0300

    upd: добавил update() в UserService

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -16,22 +16,16 @@ public class UserService {
         this.inMemoryUserStorage = inMemoryUserStorage;
     }
 
-    /**
-     * Создайте UserService, который будет отвечать за такие операции с пользователями, как
-     *  добавление в друзья,
-     *  удаление из друзей,
-     *  вывод списка общих друзей.
-     *
-     * Пока пользователям не надо одобрять заявки в друзья — добавляем сразу.
-     * То есть если Лена стала другом Саши, то это значит, что Саша теперь друг Лены.
-     */
+    public User create(User user) {
+        return inMemoryUserStorage.create(user);
+    }
+
+    public User update(User user) {
+        return inMemoryUserStorage.update(user);
+    }
 
     public List<User> findAll() {
         return inMemoryUserStorage.findAll();
-    }
-
-    public User create(User user) {
-        return inMemoryUserStorage.create(user);
     }
 
     public User remove(User user) {
