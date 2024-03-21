ac667d92310cd30bf4a38777417d29294736dcca
Kulkov_Mikhail <mk1221@yandex.ru>
2024-03-21 15:29:40 +0300

    upd: мелкие исправления

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,33 +4,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-    private InMemoryFilmStorage inMemoryFilmStorage;
+    private final FilmService filmService;
 
     @Autowired
-    public FilmController(InMemoryFilmStorage inMemoryFilmStorage) {
-        this.inMemoryFilmStorage = inMemoryFilmStorage;
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
     }
 
     @GetMapping
-    public List<Film> listAllFilms() {
-        return inMemoryFilmStorage.listAllFilms();
+    public List<Film> findAll() {
+        return filmService.findAll();
     }
 
     @PostMapping
     public Film create(@RequestBody Film film) throws ValidationException {
-        return inMemoryFilmStorage.create(film);
+        return filmService.create(film);
     }
 
     @PutMapping
     public Film update(@RequestBody Film film) throws ValidationException {
-        return inMemoryFilmStorage.update(film);
+        return filmService.update(film);
+    }
+
+    @DeleteMapping
+    public Film remove(@RequestBody Film film) throws NullPointerException {
+        return filmService.remove(film);
     }
 }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.util.*;
 
@@ -13,12 +12,10 @@ import java.util.*;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    private final InMemoryUserStorage inMemoryUserStorage;
 
     @Autowired
-    public UserController(UserService userService, InMemoryUserStorage inMemoryUserStorage) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.inMemoryUserStorage = inMemoryUserStorage;
     }
 
     @GetMapping
@@ -33,7 +30,7 @@ public class UserController {
 
     @PutMapping
     public User update(@RequestBody User user) throws ValidationException, NoSuchElementException {
-        return inMemoryUserStorage.update(user);
+        return userService.update(user);
     }
 
     @DeleteMapping
