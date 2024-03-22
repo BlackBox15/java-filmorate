e5096c0abb5ced62a6c7494fe3f01887b6d8282d
Kulkov_Mikhail <mk1221@yandex.ru>
2024-03-22 12:03:04 +0300

    upd: поправил codestyle

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NoSuchObjectException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/NoSuchObjectException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class NoSuchObjectException extends RuntimeException{
+public class NoSuchObjectException extends RuntimeException {
     public NoSuchObjectException(String message) {
         super(message);
     }
