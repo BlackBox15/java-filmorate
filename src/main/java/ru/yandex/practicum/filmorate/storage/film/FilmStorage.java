339add22eb36e4716043cbe2ebfcc18a1280ae0a
Kulkov_Mikhail <mk1221@yandex.ru>
2024-03-22 12:15:05 +0300

    upd: codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,17 +1,22 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
-
 import java.util.List;
 
 public interface FilmStorage {
     Film create(Film film);
-    Film remove(Film film);
-    Film update(Film film);
-    List<Film> findAll();
-    Film likeFilm(int filmId, int userId);
-    Film deleteLike(int filmId, int userId);
-    List<Film> findTopRated(int count);
-    List<Film> findTopRated();
 
+    Film remove(Film film);
+
+    Film update(Film film);
+
+    List<Film> findAll();
+
+    Film likeFilm(int filmId, int userId);
+
+    Film deleteLike(int filmId, int userId);
+
+    List<Film> findTopRated(int count);
+
+    List<Film> findTopRated();
 }
