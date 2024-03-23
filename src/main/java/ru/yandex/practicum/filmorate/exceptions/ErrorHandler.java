f40ebde9026ee487ca3470c15776cb0c7a0e56dd
Kulkov_Mikhail <mk1221@yandex.ru>
2024-03-23 22:28:15 +0300

    upd: рабочая версия

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
@@ -4,26 +4,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Map;
+import ru.yandex.practicum.filmorate.controllers.ErrorResponse;
 
 @RestControllerAdvice(value = "ru.yandex.practicum.controllers")
-public class errorHandler {
+public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(final ValidationException e) {
-        return Map.of("error", e.getMessage());
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        return new ErrorResponse("error", "ошибка валидации");
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleNoSuchObjectException(final NoSuchObjectException e) {
-        return Map.of("error", e.getMessage());
+    public ErrorResponse handleNoSuchObjectException(final NoSuchObjectException e) {
+        return new ErrorResponse("error", "не найден элемент");
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleException(final RuntimeException e) {
-        return Map.of("error", e.getMessage());
+    public ErrorResponse handleException(final RuntimeException e) {
+        return new ErrorResponse("error", e.getMessage());
     }
 }
