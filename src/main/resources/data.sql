4d5f50c81e6b4665919efe2212708dbbfab44e5c
Kulkov_Mikhail <mk1221@yandex.ru>
2024-04-04 21:06:09 +0300

    upd: промежуточное сохранение

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,22 @@ VALUES (1, 'G'),
        (3, 'PG-13'),
        (4, 'R'),
        (5, 'NC-17');
+
+MERGE INTO GENRE KEY(ID)
+VALUES (1, 'Comedy'),
+       (2, 'Drama'),
+       (3, 'Thriller'),
+       (4, 'Horror'),
+       (5, 'Cartoon'),
+       (6, 'Musical'),
+       (7, 'Western'),
+       (8, 'Sci-fi'),
+       (9, 'Action'),
+       (10, 'Adventure'),
+       (11, 'Biography'),
+       (12, 'Crime'),
+       (13, 'Documentary'),
+       (14, 'History'),
+       (15, 'Romance'),
+       (16, 'Sports')
+;
