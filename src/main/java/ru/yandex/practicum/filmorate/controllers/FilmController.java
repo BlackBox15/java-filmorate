eacab24435d52a43d1e67ca14913725c2a662489
Kulkov_Mikhail <mk1221@yandex.ru>
2024-04-08 10:37:53 +0300

    починил

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
