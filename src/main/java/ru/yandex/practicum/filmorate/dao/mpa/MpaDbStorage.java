495fa63b3b1642e444d78e7e683dad8a58bea7b4
Kulkov_Mikhail <mk1221@yandex.ru>
2024-04-10 03:41:36 +0300

    починил проверку по Id для MPA

--- src/main/java/ru/yandex/practicum/filmorate/dao/mpa/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/mpa/MpaDbStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.dao.mpa;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exceptions.NoSuchObjectException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
@@ -34,6 +35,10 @@ public class MpaDbStorage implements MpaStorage {
 
     @Override
     public Mpa findById(int mpaId) {
+        if (!allMpaId().contains(mpaId)) {
+            log.error("Отсутствует MPA-рейтинг");
+            throw new NoSuchObjectException("Отсутствует MPA-рейтинг");
+        }
         String sql = "select * from MPA where ID = ?";
         return jdbcTemplate.queryForObject(sql, this::mapRowMpa, mpaId);
     }
