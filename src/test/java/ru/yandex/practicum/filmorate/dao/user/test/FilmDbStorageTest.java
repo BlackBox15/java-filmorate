5d95d78911848c9214da74d973ca5c32f9675b2c
Kulkov_Mikhail <mk1221@yandex.ru>
2024-04-10 18:38:01 +0300

    тесты для filmDbStorage

--- src/test/java/ru/yandex/practicum/filmorate/dao/user/test/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/user/test/FilmDbStorageTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.dao.user.test;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -20,14 +21,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class FilmDbStorageTest {
     private final JdbcTemplate jdbcTemplate;
+    private FilmDbStorage filmDbStorage;
+    private Film film1;
+    private Film film2;
+    private Film film3;
 
-    @Test
-    public void testAllFilmIds() {
+    @BeforeEach
+    public void initCase() {
         GenreDbStorage genreDbStorage = new GenreDbStorage(jdbcTemplate);
         MpaDbStorage mpaDbStorage = new MpaDbStorage(jdbcTemplate);
-        FilmDbStorage filmDbStorage = new FilmDbStorage(jdbcTemplate, genreDbStorage, mpaDbStorage);
+        filmDbStorage = new FilmDbStorage(jdbcTemplate, genreDbStorage, mpaDbStorage);
 
-        Film film1 = new Film();
+        film1 = new Film();
         film1.setName("Ну погоди");
         film1.setDuration(50);
         film1.setReleaseDate(LocalDate.parse("1980-01-01"));
@@ -35,11 +40,17 @@ public class FilmDbStorageTest {
         mpa.setId(3);
         film1.setMpa(mpa);
 
-        Film film2 = new Film();
+        film2 = new Film();
         film2.setName("Ну погоди 2");
         film2.setDuration(50);
         film2.setReleaseDate(LocalDate.parse("1984-01-01"));
         film2.setMpa(mpa);
+
+        film3 = new Film();
+        film3.setName("Ну погоди 3");
+        film3.setDuration(87);
+        film3.setReleaseDate(LocalDate.parse("1988-01-01"));
+        film3.setMpa(mpa);
 
         Film checkedFilm1 = filmDbStorage.create(film1);
         film1.setId(checkedFilm1.getId());
@@ -47,94 +58,65 @@ public class FilmDbStorageTest {
         Film checkedFilm2 = filmDbStorage.create(film2);
         film2.setId(checkedFilm2.getId());
 
-        List<Integer> actualIds = List.of(film1.getId(), film2.getId());
+        Film checkedFilm3 = filmDbStorage.create(film3);
+        film3.setId(checkedFilm3.getId());
+    }
+
+    @Test
+    public void testAllFilmIds() {
+        List<Integer> actualIds = List.of(film1.getId(), film2.getId(), film3.getId());
         List<Integer> checkedIds = filmDbStorage.allFilmId();
+
         assertThat(checkedIds).isEqualTo(actualIds);
     }
 
     @Test
     public void testAllFilmNames() {
-        GenreDbStorage genreDbStorage = new GenreDbStorage(jdbcTemplate);
-        MpaDbStorage mpaDbStorage = new MpaDbStorage(jdbcTemplate);
-        FilmDbStorage filmDbStorage = new FilmDbStorage(jdbcTemplate, genreDbStorage, mpaDbStorage);
+        List<String> actualFilmNames = List.of(film1.getName(), film2.getName(), film3.getName());
+        List<String> checkedFilmNames = filmDbStorage.allFilmNames();
 
-        Film film1 = new Film();
-        film1.setName("Ну погоди");
-        film1.setDuration(50);
-        film1.setReleaseDate(LocalDate.parse("1980-01-01"));
-        Mpa mpa = new Mpa();
-        mpa.setId(3);
-        film1.setMpa(mpa);
-
-        Film film2 = new Film();
-        film2.setName("Ну погоди 2");
-        film2.setDuration(50);
-        film2.setReleaseDate(LocalDate.parse("1984-01-01"));
-        film2.setMpa(mpa);
-
-        Film checkedFilm1 = filmDbStorage.create(film1);
-        film1.setId(checkedFilm1.getId());
-
-        Film checkedFilm2 = filmDbStorage.create(film2);
-        film2.setId(checkedFilm2.getId());
-
-        List<String> allFilmNames = filmDbStorage.allFilmNames();
-        assertThat(allFilmNames.get(0)).isEqualTo(film1.getName());
-        assertThat(allFilmNames.get(1)).isEqualTo(film2.getName());
+        assertThat(checkedFilmNames).isEqualTo(actualFilmNames);
     }
 
     @Test
     public void testCreateFilm() {
-        GenreDbStorage genreDbStorage = new GenreDbStorage(jdbcTemplate);
-        MpaDbStorage mpaDbStorage = new MpaDbStorage(jdbcTemplate);
-        FilmDbStorage filmDbStorage = new FilmDbStorage(jdbcTemplate, genreDbStorage, mpaDbStorage);
-
-        Film film1 = new Film();
-        film1.setName("Ну погоди");
-        film1.setDuration(50);
-        film1.setReleaseDate(LocalDate.parse("1980-01-01"));
+        Film anotherFilm = new Film();
+        anotherFilm.setName("Какой-то странный фильм, ужастик, и это его название");
+        anotherFilm.setDuration(187);
+        anotherFilm.setReleaseDate(LocalDate.parse("2000-01-01"));
         Mpa mpa = new Mpa();
-        mpa.setId(3);
-        film1.setMpa(mpa);
+        mpa.setId(5);
+        anotherFilm.setMpa(mpa);
 
-        Film checkedFilm1 = filmDbStorage.create(film1);
-        film1.setId(checkedFilm1.getId());
+        int oldSizeDbStorage = filmDbStorage.findAll().size();
 
-        assertThat(filmDbStorage.findAll().size()).isEqualTo(1);
-        assertThat(filmDbStorage.findAll().get(0).getName()).isEqualTo(film1.getName());
-        assertThat(filmDbStorage.findAll().get(0).getDuration()).isEqualTo(film1.getDuration());
-        assertThat(filmDbStorage.findAll().get(0).getId()).isEqualTo(film1.getId());
-        assertThat(filmDbStorage.findAll().get(0).getReleaseDate()).isEqualTo(film1.getReleaseDate());
+        Film checkedFilm1 = filmDbStorage.create(anotherFilm);
+        anotherFilm.setId(checkedFilm1.getId());
+
+        assertThat(filmDbStorage.findAll().size()).isEqualTo(oldSizeDbStorage + 1);
+        assertThat(filmDbStorage.findAll().contains(anotherFilm)).isTrue();
     }
 
     @Test
     public void testUpdateFilm() {
-        GenreDbStorage genreDbStorage = new GenreDbStorage(jdbcTemplate);
-        MpaDbStorage mpaDbStorage = new MpaDbStorage(jdbcTemplate);
-        FilmDbStorage filmDbStorage = new FilmDbStorage(jdbcTemplate, genreDbStorage, mpaDbStorage);
+        Film filmToUpdate = filmDbStorage.findAll().get(filmDbStorage.findAll().size() - 1);
+        filmToUpdate.setName("Обновлённое название");
+        filmDbStorage.update(filmToUpdate);
 
-        Film film1 = new Film();
-        film1.setName("Ну погоди");
-        film1.setDuration(50);
-        film1.setReleaseDate(LocalDate.parse("1980-01-01"));
-        Mpa mpa = new Mpa();
-        mpa.setId(3);
-        film1.setMpa(mpa);
-        Film oldFilm = filmDbStorage.create(film1);
-        film1.setId(oldFilm.getId());
-
-        film1.setReleaseDate(LocalDate.parse("1980-03-03"));
-        filmDbStorage.update(film1);
-
-        assertThat(filmDbStorage.findAll().get(0).getName()).isEqualTo(oldFilm.getName());
-        assertThat(filmDbStorage.findAll().get(0).getDescription()).isEqualTo(oldFilm.getDescription());
-        assertThat(filmDbStorage.findAll().get(0).getDuration()).isEqualTo(oldFilm.getDuration());
-        assertThat(filmDbStorage.findAll().get(0).getReleaseDate()).isNotEqualTo(oldFilm.getReleaseDate());
-        assertThat(filmDbStorage.findAll().get(0).getReleaseDate()).isEqualTo(film1.getReleaseDate());
+        assertThat(filmDbStorage.findAll().contains(filmToUpdate)).isTrue();
+        assertThat(filmDbStorage.findAll().stream().anyMatch(filmToUpdate::equals)).isTrue();
     }
 
     @Test
     public void testDeleteFilm() {
+        Film filmToDelete = filmDbStorage.findAll().get(filmDbStorage.findAll().size() - 1);
+        assertThat(filmDbStorage.findAll().contains(filmToDelete)).isTrue();
+        filmDbStorage.remove(filmToDelete);
+        assertThat(filmDbStorage.findAll().contains(filmToDelete)).isFalse();
+    }
+
+    @Test
+    public void testFindAllFilms() {
         GenreDbStorage genreDbStorage = new GenreDbStorage(jdbcTemplate);
         MpaDbStorage mpaDbStorage = new MpaDbStorage(jdbcTemplate);
         FilmDbStorage filmDbStorage = new FilmDbStorage(jdbcTemplate, genreDbStorage, mpaDbStorage);
@@ -152,20 +134,12 @@ public class FilmDbStorageTest {
         film2.setDuration(50);
         film2.setReleaseDate(LocalDate.parse("1984-01-01"));
         film2.setMpa(mpa);
-
         Film checkedFilm1 = filmDbStorage.create(film1);
         film1.setId(checkedFilm1.getId());
 
         Film checkedFilm2 = filmDbStorage.create(film2);
         film2.setId(checkedFilm2.getId());
 
-        filmDbStorage.remove(film1);
 
-        assertThat(filmDbStorage.findAll().size()).isEqualTo(1);
-        assertThat(filmDbStorage.findAll().get(0).getName()).isEqualTo(film2.getName());
-        assertThat(filmDbStorage.findAll().get(0).getDuration()).isEqualTo(film2.getDuration());
-        assertThat(filmDbStorage.findAll().get(0).getId()).isEqualTo(film2.getId());
-        assertThat(filmDbStorage.findAll().get(0).getReleaseDate()).isEqualTo(film2.getReleaseDate());
     }
-
 }
